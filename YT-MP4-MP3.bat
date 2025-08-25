3794b92b6a4f851eba93bfb7354f48cbd30e3468
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-08-24 20:22:48 -0500

    Update YT-MP4-MP3.bat

--- YT-MP4-MP3.bat
+++ YT-MP4-MP3.bat
@@ -1,5 +1,4 @@
 @echo off
-
 REM Prompt user input
 set /p mediaURL=Enter media URL: 
 set /p type=Is this a video or audio? (v/a): 
@@ -10,15 +9,14 @@ set filename=download_%timestamp%
 
 if /i "%type%"=="v" (
     echo [+] Downloading video...
-    yt-dlp -o "%filename%.mp4" --format mp4 "%mediaURL%"
+    yt-dlp -o "%filename%.mp4" -f "bv*+ba" --merge-output-format mp4 --user-agent "Mozilla/5.0" "%mediaURL%"
 ) else if /i "%type%"=="a" (
     echo [+] Downloading audio...
-    yt-dlp -x --audio-format mp3 -o "%filename%.mp3" "%mediaURL%"
+    yt-dlp -x --audio-format mp3 -o "%filename%.mp3" --user-agent "Mozilla/5.0" "%mediaURL%"
 ) else (
     echo Invalid input. Use 'v' for video or 'a' for audio.
     exit /b 1
 )
 
 echo.
-
 echo [✓] Done.
