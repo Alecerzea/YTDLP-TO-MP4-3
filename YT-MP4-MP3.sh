9f19a9fcde408163aad73e4059894cce05591387
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-10-13 19:17:47 -0500

    Update YT-MP4-MP3.sh

--- YT-MP4-MP3.sh
+++ YT-MP4-MP3.sh
@@ -3,19 +3,13 @@
 read -rp "Enter media URL: " mediaURL
 read -rp "Is this a video or audio? (v/a): " type
 
-timestamp=$(date +%s)
-filename="download_$timestamp"
-
 if [ "$type" = "v" ]; then
     echo "[+] Downloading video..."
-    yt-dlp -o "${filename}.mp4" -f "bv*+ba" --merge-output-format mp4 --user-agent "Mozilla/5.0" "$mediaURL"
+    yt-dlp -f "bv*+ba/b" --merge-output-format mp4 -o "%(title)s.%(ext)s" --user-agent "Mozilla/5.0" "$mediaURL"
 elif [ "$type" = "a" ]; then
     echo "[+] Downloading audio..."
-    yt-dlp -x --audio-format mp3 -o "${filename}.mp3" --user-agent "Mozilla/5.0" "$mediaURL"
+    yt-dlp -x --audio-format mp3 -o "%(title)s.%(ext)s" --user-agent "Mozilla/5.0" "$mediaURL"
 else
     echo "Invalid input. Use 'v' for video or 'a' for audio."
     exit 1
 fi
-
-echo
-echo "[✓] Done."
