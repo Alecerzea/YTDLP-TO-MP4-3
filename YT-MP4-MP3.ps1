83912c442678cbd5f0dcdd6d6cf045f7d287b474
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-10-13 19:17:29 -0500

    Update YT-MP4-MP3.ps1

--- YT-MP4-MP3.ps1
+++ YT-MP4-MP3.ps1
@@ -1,17 +1,13 @@
 $mediaURL = Read-Host "Enter media URL"
 $type = Read-Host "Is this a video or audio? (v/a)"
-$timestamp = Get-Date -UFormat %s
-$filename = "download_$timestamp"
 
 if ($type -eq "v") {
     Write-Host "[+] Downloading video..."
-    yt-dlp -o "${filename}.mp4" -f "bv*+ba" --merge-output-format mp4 --user-agent "Mozilla/5.0" $mediaURL
+    yt-dlp -f "bv*+ba/b" --merge-output-format mp4 -o "%(title)s.%(ext)s" --user-agent "Mozilla/5.0" $mediaURL
 } elseif ($type -eq "a") {
     Write-Host "[+] Downloading audio..."
-    yt-dlp -x --audio-format mp3 -o "${filename}.mp3" --user-agent "Mozilla/5.0" $mediaURL
+    yt-dlp -x --audio-format mp3 -o "%(title)s.%(ext)s" --user-agent "Mozilla/5.0" $mediaURL
 } else {
     Write-Host "Invalid input. Use 'v' for video or 'a' for audio."
     exit 1
 }
-
-Write-Host "`n[✓] Done."
