70fbec53ca640bd23f1136ddd78f01fac18356b9
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-10-21 20:30:16 -0500

    Update YT-MP4-MP3.bat

--- YT-MP4-MP3.bat
+++ YT-MP4-MP3.bat
@@ -4,7 +4,8 @@ set /p type=Is this a video or audio? (v/a):
 
 if /i "%type%"=="v" (
     echo [+] Downloading video...
-    yt-dlp -f "bv*+ba/b" --merge-output-format mp4 -o "%(title)s.%(ext)s" --user-agent "Mozilla/5.0" "%mediaURL%"
+    yt-dlp --merge-output-format mp4 --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.118 Safari/537.36"
+"%mediaURL%"
 ) else if /i "%type%"=="a" (
     echo [+] Downloading audio...
     yt-dlp -x --audio-format mp3 -o "%(title)s.%(ext)s" --user-agent "Mozilla/5.0" "%mediaURL%"
@@ -14,3 +15,4 @@ if /i "%type%"=="v" (
 )
 
 pause
+
