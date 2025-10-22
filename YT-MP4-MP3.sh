6b7a847ad057c4743fc77f7f542d576ca359c73e
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-10-21 20:45:44 -0500

    Update YT-MP4-MP3.sh

--- YT-MP4-MP3.sh
+++ YT-MP4-MP3.sh
@@ -5,7 +5,7 @@ read -rp "Is this a video or audio? (v/a): " type
 
 if [ "$type" = "v" ]; then
     echo "[+] Downloading video..."
-    yt-dlp -f "bv*+ba/b" --merge-output-format mp4 -o "%(title)s.%(ext)s" --user-agent "Mozilla/5.0" "$mediaURL"
+    yt-dlp --merge-output-format mp4 --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.118 Safari/537.36" "$mediaURL"
 elif [ "$type" = "a" ]; then
     echo "[+] Downloading audio..."
     yt-dlp -x --audio-format mp3 -o "%(title)s.%(ext)s" --user-agent "Mozilla/5.0" "$mediaURL"
